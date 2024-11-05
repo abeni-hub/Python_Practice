a01587a0673d59c6618b3251aff87a05c7009a44
Abenezer Sileshi <138670844+abeni-hub@users.noreply.github.com>
2024-11-05 04:51:19 +0300

    Create Nested_loop.py
    any nested loop work like this...and we can understand in this way

---
+++ Nested_loop.py
@@ -0,0 +1,15 @@
+for i in range(5):
+  for j in range(2):
+    print(f"i:{i} and j:{j}")
+
+# output of the above program
+i:0 and j:0
+i:0 and j:1
+i:1 and j:0
+i:1 and j:1
+i:2 and j:0
+i:2 and j:1
+i:3 and j:0
+i:3 and j:1
+i:4 and j:0
+i:4 and j:1
