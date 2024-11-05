2444cb9a0ab08af9cabdbbac10b993e7d7b35674
Abenezer Sileshi <138670844+abeni-hub@users.noreply.github.com>
2024-11-05 04:45:10 +0300

    Update pass.py

--- pass.py
+++ pass.py
@@ -1,1 +1,7 @@
+# Pass is a control statement and it is a null operation , it does nothing.
+for i in range(5):
+  if i==3:
+    pass
+  print(i)
 
+# its output is 0,1,2,3,4     which means (Pass) Control statement does nothing
