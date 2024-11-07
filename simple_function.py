7d94330a831e6a25bb9a374fb2863d77137e2401
Abenezer Sileshi <138670844+abeni-hub@users.noreply.github.com>
2024-11-07 05:25:49 +0300

    Create simple_function.py
    even odd and add two numbers by using function

---
+++ simple_function.py
@@ -0,0 +1,15 @@
+# Why Functions
+def even_or_odd(num):
+    """ This functions find even or odd"""
+    if num%2 ==0:
+        print("The number is even")
+    else:
+        print("The number is odd")  
+        
+even_or_odd(9)          
+
+# Add two numbers by using functions
+def addition(c,d):
+    return c+d
+result = addition(2,3)
+print(result)
