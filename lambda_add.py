2ebc84c4619f9415cc1e0feaa7eb173f5696b910
Abenezer Sileshi <138670844+abeni-hub@users.noreply.github.com>
2024-11-16 03:58:08 +0300

    Create lambda_add.py
    to functions functionality by using lambda functions
    
    Lambda function are small anonymous functions defined using the lambda keyword.

---
+++ lambda_add.py
@@ -0,0 +1,12 @@
+# add two numbers by using lambda
+# To add two numbers by using lambda functions
+addition = lambda x,y: x+y
+addition(2,3)
+
+# even number by using lambda functions
+even1 = lambda num:num%2==0
+even1(12)
+
+# add numbers with three parameters 
+addition2 = lambda x,y,z : x+y+z
+addition2(1,2,4)
